fdac8ae75ab38d47909cf104f306f49e774fe1f1
Edith Raie <edithraie@gmail.com>
2022-10-28 09:15:55 +0300

    My first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+	header: {
+		height: 220,
+		width: "100%",
+		display: "flex",
+		flexDirection: "row",
+		margine: 0,
+	},
+});
+
+const Header: React.FC = () => {
+	const classes = useStyles();
+
+	return (
+		<div className="headerImage">
+			<div>
+				<img src="images/Header.png" className={classes.header} />
+			</div>
+		</div>
+	);
+};
+
+export default Header;
